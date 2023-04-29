dbf78741fa0b28994fa155d787376257aa7ff190
zubzerozD <sebastian.canales1901@alumnos.ubiobio.cl>
2023-04-29 18:58:04 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import axios from "axios";
-import { Box, Button, Grid, Paper, Typography, Avatar, ListItem, Container, CircularProgress } from '@mui/material';
-
-import logo from './img/dog.png'
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import { Box, Button, Grid, Paper, Typography } from '@mui/material'
 
 function App() {
-  const [imgdog, setImgdog] = useState('');
-  const [rejectedDogs, setRejectedDogs] = useState([]);
-  const [acceptedDogs, setAcceptedDogs] = useState([]);
-  const [dogBreeds, setDogBreeds] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [dogBreeds, setDogBreeds] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [rejectedDogs, setRejectedDogs] = useState([])
+  const [acceptedDogs, setAcceptedDogs] = useState([])
+
+  const [dog, setDog] = useState({
+    name: "",
+    image: "",
+    description: ""
+  });
 
   useEffect(() => {
     setLoading(true);
@@ -24,130 +27,67 @@ function App() {
 
   const getDog = () => {
     setLoading(true);
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then((res) => res.json())
-      .then((data) => {
-        setImgdog(data.message);
+    axios.get('https://dog.ceo/api/breeds/image/random')
+      .then((res) => {
+        setDog({
+          name: dogBreeds[Math.floor(Math.random() * dogBreeds.length)].substring(0, 6),
+          image: res.data.message,
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam sit amet aliquet luctus, nisl nunc aliquam nunc, nec aliquam nisl nunc quis nisl. Sed euismod, diam sit amet aliquet luctus, nisl nunc aliquam nunc, nec aliquam nisl nunc quis nisl."
+        });
         setLoading(false);
       });
-  };
-
-  const spinner = (
-    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-      <CircularProgress />
-    </Box>
-  );
-
+  }
 
   useEffect(() => {
-    getDog();
-  }, []);
+    getDog()
+  }, [])
 
   const handleReject = () => {
-    const randomBreed = dogBreeds[Math.floor(Math.random() * dogBreeds.length)].substring(0, 6);
-    const rejectedDog = { name: randomBreed, image: imgdog };
-    setRejectedDogs([...rejectedDogs, rejectedDog]);
-    getDog();
-  };
+    setRejectedDogs([...rejectedDogs, dog])
+    getDog()
+  }
 
   const handleAccept = () => {
-    const randomBreed = dogBreeds[Math.floor(Math.random() * dogBreeds.length)].substring(0, 6);
-    const acceptedDog = { name: randomBreed, image: imgdog };
-    setAcceptedDogs([...acceptedDogs, acceptedDog]);
-    getDog();
-  };
-
-  const moveFromRejectedToAccepted = (dog) => {
-    const newRejectedDogs = rejectedDogs.filter((d) => d !== dog);
-    setRejectedDogs(newRejectedDogs);
-    setAcceptedDogs([...acceptedDogs, dog]);
-  };
-
-  const moveFromAcceptedToRejected = (dog) => {
-    const newAcceptedDogs = acceptedDogs.filter((d) => d !== dog);
-    setAcceptedDogs(newAcceptedDogs);
-    setRejectedDogs([...rejectedDogs, dog]);
-  };
-
-  const content = (
-    <>
-      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 1 }}>
-        <img src={imgdog} alt="dog" style={{ width: '200px', height: 200 }} />
-      </Box>
-      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 1 }}>
-        <Button variant="contained" onClick={handleReject} sx={{ mr: 2 }}>
-          Rechazar
-        </Button>
-        <Button variant="contained" onClick={handleAccept}>
-          Match
-        </Button>
-      </Box>
-    </>
-  );
+    setAcceptedDogs([...acceptedDogs, dog])
+    getDog()
+  }
 
   return (
-    <Container className="background">
-      <Grid container spacing={3}>
-        <Grid style={{}} item xs={12} sm={4}>
-          <Paper style={{ opacity: 0.9, borderRadius: 25, height: 555, width: 165 }} sx={{ p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', mt: 7 }}>
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: -2 }}>
-              <img className="logo" src={logo} alt="zd" />
-            </Box>
-            <Typography variant="h5" gutterBottom>
-              Encuentra tu perro
-            </Typography>
-            {loading ? spinner : content}
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <ListItem className="list-item">
-            <Paper style={{ opacity: 0.9, height: 550, overflowY: "auto", borderRadius: 25 }} sx={{ p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', mt: 7 }}>
-              <Typography variant="h5" gutterBottom>
-                Aceptados
-              </Typography>
-              {acceptedDogs.map((dog) => (
-                <Box key={dog.image} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 1 }}>
-                  <Avatar
-                    src={dog.image}
-                    sx={{ width: 100, height: 100 }}
-                    alt="Imagen Avatar"
-                    style={{ marginRight: "10px" }}
-                  />
-                  <Typography variant="subtitle1" sx={{ mt: 1 }}>
-                    {dog.name}
-                  </Typography>
-                  <Button onClick={() => moveFromAcceptedToRejected(dog)}>Rechazar</Button>
-                </Box>
-              ))}
-            </Paper>
-          </ListItem>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <ListItem className="list-item">
-            <Paper style={{ opacity: 0.9, height: 550, overflowY: "auto", borderRadius: 25 }} sx={{ p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', mt: 7 }}>
-              <Typography variant="h5" gutterBottom>
-                Rechazados
-              </Typography>
-              {rejectedDogs.map((dog) => (
-                <Box key={dog.image} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 1 }}>
-                  <Avatar
-                    src={dog.image}
-                    sx={{ width: 100, height: 100 }}
-                    alt="Imagen Avatar"
-                    style={{ marginRight: "10px" }}
-                  />
-                  <Typography variant="subtitle1" sx={{ mt: 1 }}>
-                    {dog.name}
-                  </Typography>
-                  <Button onClick={() => moveFromRejectedToAccepted(dog)}>Match</Button>
-                </Box>
-              ))}
-            </Paper>
-          </ListItem>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Box sx={{ width: '30%', minWidth: '250px', mr: 2 }}>
+        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%' }}>
+          <Typography variant="h5" gutterBottom>
+            Rechazados
+          </Typography>
+        </Paper>
+      </Box>
+      <Box sx={{ width: '70%', minWidth: '250px' }}>
+        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%' }}>
+          <Typography variant="h5" gutterBottom>
+            Encuentra tu perro
+          </Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 1 }}>
+            <img src={dog.image} alt="dog" style={{ width: '250px' }} />
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 1 }}>
+            <Button variant="contained" onClick={handleReject} sx={{ mr: 2 }}>
+              Rechazar
+            </Button>
+            <Button variant="contained" onClick={handleAccept}>
+              Match
+            </Button>
+          </Box>
+        </Paper>
+      </Box>
+      <Box sx={{ width: '30%', minWidth: '250px', ml: 2 }}>
+        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%' }}>
+          <Typography variant="h5" gutterBottom>
+            Aceptados
+          </Typography>
+        </Paper>
+      </Box>
+    </Box>
+  )
 }
 
 export default App
